213db6abaacd25a060b5504822fd7d22fd2c2964
MoustapahaBalde621 <moustaphakessobalde@gmail.com>
2025-05-31 20:41:58 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navbar from './components/dashbord/Navbar'
 import './App.css'
 import Test from "./Test"
 
