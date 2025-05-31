836b849e41a063db69c73b9ac44baf9d6b06f113
drbea <beavoguimaximeakoi@gmail.com>
2025-05-31 19:12:08 +0000

    Creation du fichier Register pour s'inscrire

---
+++ frontend/src/components/dashbord/Dashboard.jsx
@@ -0,0 +1,17 @@
+
+
+
+
+import Logo from "./Logo"
+import Menugauche from "./Menugauche"
+function Dashboard() {
+
+  return (
+   <div>
+      <Menugauche/>
+      <Logo/>
+   </div>
+  )
+}
+
+export default Dashboard
