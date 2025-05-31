ab315f691b298a556fe5fbbefee338d3d415c5ba
drbea <beavoguimaximeakoi@gmail.com>
2025-05-31 17:25:03 +0000

    Ajout de la barre pour le logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import './App.css'
+import Test from "./Test"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-   <h1>hello word</h1>
+    <div>
+   <Test />
+    </div>
+
   )
 }
 
-export default App
+export default App;
