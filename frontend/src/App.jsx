1bbaee91d396c569a1926d288d42cefc5f93754a
drbea <beavoguimaximeakoi@gmail.com>
2025-06-05 12:14:50 +0000

    fix retrait de la page panier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Test from "./Test"
 import {BrowserRouter,Routes,Route} from "react-router-dom" 
 import Vente from "./components/pages/Ventes"
 import Stocks from "./components/pages/Stocks"
-import Panier from "./components/pages/Panier"
 import LandingPage from './components/pages/LandingPage'
 import Home from "./components/pages/Home"
 import GererStock from "./components/products/GererStock"
@@ -26,13 +25,11 @@ function App() {
     <Route path='/home'element={<Home/>}></Route>
     <Route path='/ventes'element={<Vente/>}></Route>
     <Route path='/stocks'element={<Stocks/>}></Route>
-    <Route path='/panier'element={<Panier/>}></Route>
     <Route path='/register' element={<Register/>}></Route>
     <Route path='/login' element={<Login/>}></Route>
     <Route path='/products' element={<Product/>}></Route>
     <Route path='/ajoutproduit' element={<AjouterProduit/>}></Route>
     <Route path='/ajoutvente' element={<EnregistrerVente/>}></Route>
-    <Route path='/panier' element={<Panier/>}></Route>
    </Routes>
   
    </BrowserRouter>
