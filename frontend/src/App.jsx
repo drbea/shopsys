3bb9284b3924715e31436354daeedea068632591
MoustapahaBalde621 <moustaphakessobalde@gmail.com>
2025-06-05 16:22:41 +0200

    AA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Profil from './components/profil/Profil'
 import './App.css'
 import Test from "./Test"
+
 
 function App() {
 
